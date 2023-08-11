0319f3adaf81090c2b7eb919521bad74c494033b
linsalrob <raedwards@gmail.com>
2023-08-11 10:16:46 +0930

    adding DEFINITIONS files

---
+++ slurm/DEFINITIONS_nohost.sh
@@ -0,0 +1,8 @@
+# this definitions file is used if there is no host.
+# You can just skip the host removal step, and then we continue using the HOSTREMOVED directory, which is
+# our cleaned files
+
+export FILEEND=_L001_R1_001.fastq.gz
+export FAFILEEND=_L001_R1_001.fasta.gz
+export SOURCE=fastq
+export HOSTREMOVED=fastq_fastp
---
+++ slurm/DEFINITIONS_shark.sh
@@ -0,0 +1,9 @@
+# This definitions file is for use with the SHARK genomes. 
+# Make sure you change FILEEND/FAFILEEND
+
+export FILEEND=_R1_001.fastq.gz
+export FAFILEEND=_R1_001.fasta.gz
+export SOURCE=fastq
+export HOSTFILE=/home/nala0006/Ref-Shark-Genomes/Ref-SharkAll.fna
+export HOST=shark
+export HOSTREMOVED=no_shark

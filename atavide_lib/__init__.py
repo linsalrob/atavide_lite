3b789b33f240d8965d1a5f0325f470a4a944dacd
linsalrob <raedwards@gmail.com>
2025-04-01 08:00:22 +1030

    adding stream_fasta

--- atavide_lib/__init__.py
+++ atavide_lib/__init__.py
@@ -3,9 +3,9 @@ import sys
 
 __author__ = 'Rob Edwards'
 from .colours import colours, colors, message
-from .sequences import stream_fastq
+from .sequences import stream_fastq, stream_fasta
 from .read_definitions import read_definitions
 
 __all__ = [
-    'colours', 'colors', 'message', 'stream_fastq', 'read_definitions'
+    'colours', 'colors', 'message', 'stream_fastq', 'stream_fasta', 'read_definitions'
     ]

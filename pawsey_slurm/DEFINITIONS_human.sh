e133caaf55d3424d533ac1afd998601638db0d4c
linsalrob <raedwards@gmail.com>
2025-04-12 05:53:36 +0800

    pawsifying

---
+++ pawsey_slurm/DEFINITIONS_human.sh
@@ -0,0 +1,9 @@
+# This definitions file is for use with the HUMAN genomes. 
+# Make sure you change FILEEND/FAFILEEND
+
+export FILEEND=_R1.fastq.gz
+export FAFILEEND=_R1.fasta.gz
+export SOURCE=fastq
+export HOSTFILE=/scratch/$PAWSEY_PROJECT/$USER/Databases/human/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz
+export HOST=human
+export HOSTREMOVED=no_human

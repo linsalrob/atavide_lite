c05412424ffff2dbdefea724f9aeb23388537bf3
linsalrob <raedwards@gmail.com>
2025-04-24 16:54:29 +0800

    DEFINTIONS for single end

---
+++ pawsey_minion/DEFINITIONS_human.sh
@@ -0,0 +1,10 @@
+# This definitions file is for use with the HUMAN genomes. 
+# Make sure you change FILEEND/FAFILEEND
+
+export SAMPLENAME=xxxyyy
+export FILEEND=.fastq.gz
+export FAFILEEND=.fasta.gz
+export SOURCE=fastq
+export HOSTFILE=/scratch/$PAWSEY_PROJECT/$USER/Databases/human/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz
+export HOST=human
+export HOSTREMOVED=no_human

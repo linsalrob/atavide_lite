a35133f0efe7afa28f26052f2df434982f710048
linsalrob <raedwards@gmail.com>
2025-06-20 15:13:19 +1000

    updating cutadapt

--- nci_pbs/fasta/cutadapt_submit.sh
+++ nci_pbs/fasta/cutadapt_submit.sh
@@ -8,10 +8,19 @@ fi
 
 SOURCE=$1
 DEST=cutadapt
-SCRIPT=~/GitHubs/atavide_lite/pbs/fasta/cutadapt.pbs
+SCRIPT=$HOME/GitHubs/atavide_lite/nci_pbs/fasta/cutadapt.pbs
 
-for FILE in $(find $SOURCE -type f -printf "%f\n"); do 
-	if [[ ! -e $DEST/$FILE ]]; then
+if [[ ! -e $SCRIPT ]]; then 
+	echo "Executable $SCRIPT not found!" >&2;
+	exit 1;
+fi
+
+mkdir -p $DEST
+
+for FILE in $(find $SOURCE -type f -name \*_R1\* -printf "%f\n"); do 
+	if [[ -e $SOURCE/${FILE/R1/R2} ]] && [[ ! -e $DEST/$FILE ]]; then
+		qsub -v R1=$SOURCE/$FILE,R2=$SOURCE/${FILE/R1/R2} $SCRIPT
+	elif [[ ! -e $DEST/$FILE ]]; then
 		qsub -v R1=$SOURCE/$FILE $SCRIPT
 	fi
 done

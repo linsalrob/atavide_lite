9c701508ed928fc2e5bd954303144bec6d47c212
linsalrob <raedwards@gmail.com>
2025-06-20 15:42:50 +1000

    updating to R1 and R2

--- nci_pbs/fasta/human_submit.sh
+++ nci_pbs/fasta/human_submit.sh
@@ -8,9 +8,11 @@ fi
 
 SOURCE=$1
 DEST=human
-SCRIPT=~/GitHubs/atavide_lite/pbs/fasta/human.pbs
-for FILE in $(find $SOURCE -name \*_R1* -printf "%f\n"); do 
-	if [[ ! -e $DEST/$FILE ]]; then
+SCRIPT=~/GitHubs/atavide_lite/nci_pbs/fasta/human.pbs
+for FILE in $(find $SOURCE -type f -name \*_R1\* -printf "%f\n"); do 
+	if [[ -e $SOURCE/${FILE/R1/R2} ]] && [[ ! -e $DEST/$FILE ]]; then
+		qsub -v R1=$SOURCE/$FILE,R2=$SOURCE/${FILE/R1/R2} $SCRIPT
+	elif [[ ! -e $DEST/$FILE ]]; then
 		qsub -v R1=$SOURCE/$FILE $SCRIPT
 	fi
 done

f5ce309d4603440e0199caf476467ce7e078e991
linsalrob <raedwards@gmail.com>
2025-08-29 10:32:49 +0800

    adding a mouse host

---
+++ pawsey_shortread/DEFINITIONS_mouse.sh
@@ -0,0 +1,10 @@
+# This definitions file is for use with the HUMAN genomes. 
+# Make sure you change FILEEND/FAFILEEND
+
+export SAMPLENAME=xxxyyy
+export FILEEND=_R1.fastq.gz
+export FAFILEEND=_R1.fasta.gz
+export SOURCE=fastq
+export HOSTFILE=/scratch/$PAWSEY_PROJECT/$USER/Databases/mouse/GCF_000001635.27_GRCm39_genomic.fna.gz
+export HOST=mouse
+export HOSTREMOVED=no_mouse

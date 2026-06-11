1f5f5f1ee7b1f70facdfa4894c6b0ca518d26137
linsalrob <raedwards@gmail.com>
2026-06-11 15:36:11 +0800

    removing unnecessary FAFILEEND

---
+++ pawsey_shortread/DEFINITIONS_nohost.sh
@@ -0,0 +1,8 @@
+# this definitions file is used if there is no host.
+# You can just skip the host removal step, and then we continue using the HOSTREMOVED directory, which is
+# our cleaned files
+
+export SAMPLENAME=xxxyyy
+export FILEEND=_L001_R1_001.fastq.gz
+export SOURCE=fastq
+export HOSTREMOVED=fastq_fastp
